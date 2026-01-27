c964060949f5ba0bdbe2d8c73142acd8d6e52490
anushkagurbe <argurbe@gmail.com>
2026-01-27 18:52:21 +0530

    created authMiddleware

---
+++ backend/middlewares/authMiddlewares.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+export let verifyJwtMiddleware = async (req, res, next) =>{
+    try
+    {
+        let token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+        if(!token)
+        {
+            return res.status(401).json({ success: false, message: "Token not found" });
+        }
+        let decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        if(!decodedToken)
+        {
+            return res.status(403).json({ success: false, message: "Invalid token" });
+        }
+        let user = await userModel.findOne({_id: decodedToken._id},{ username: 1, email: 1, fullName: 1, role: 1 });
+        req.user = user;
+        next();
+    }
+    catch(error)
+    {
+        console.log(error);
+        return res.status(500).json({success: false, message: "Internal server error"})
+    }
+}
